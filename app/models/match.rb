34c2984dd96e787a7d20fde2e6f2db58bd252aaa
sema karan <karan18sema@gmail.com>
2024-03-04 16:28:00 +0100

    create model associations validations

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,5 @@
 class Match < ApplicationRecord
   belongs_to :goal
   belongs_to :user
+  has_many :tasks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :goals
+  has_many :tasks
+  has_many :matches
+
+  has_many :received_matches, through: :goals, source: :user
+  validates :first_name, :last_name, presence: true
 end
