94e28e1061876fd986c7fd31c130640577d37b40
allison.xuan.chen@gmail.com <allison.xuan.chen@gmail.com>
2024-03-05 16:57:34 +0100

    goals controller create

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,33 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def index
+    @goals = Goal.all
+  end
+
+  def show
+    @goal = Goal.find(params[:id])
+  end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.user = @user
+    if @goal.save
+      redirect_to goals_path(@goal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def goal_params
+    params.require(:goal).permit(:category, :description)
+  end
+
 end
