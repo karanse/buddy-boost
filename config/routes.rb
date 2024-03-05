8fe86e96f7fefd6f754e8f917589d9d950dfef06
sema karan <karan18sema@gmail.com>
2024-03-05 12:45:32 +0100

    add page folders

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "pages#home"
+  get '/legal', to: "pages#legal"
+  get '/profile', to: "pages#profile"
+  resources :goals, only: [:new, :create, :show, :index] do
+    resources :matches, only: [:new, :create, :show, :edit, :update] do
+      resources :tasks, only: [:new, :create, :index, :edit, :update]
+    end
+  end
 end
