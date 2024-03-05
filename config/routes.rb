5ba7bea720ea45ab1d12d523c11910d6d465b9dd
sema karan <karan18sema@gmail.com>
2024-03-05 14:24:31 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,18 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   root "pages#home"
   get '/legal', to: "pages#legal"
   get '/profile', to: "pages#profile"
+
   resources :goals, only: [:new, :create, :show, :index] do
-    resources :matches, only: [:new, :create, :show, :edit, :update] do
-      resources :tasks, only: [:new, :create, :index, :edit, :update]
-    end
+    resources :matches, only: [:new, :create]
   end
+  resouces :matches, only: [:show, :edit, :update] do
+    resources :tasks, only: [:new, :create]
+  end
+
+  resources :tasks, only: [:edit, :update]
 end
