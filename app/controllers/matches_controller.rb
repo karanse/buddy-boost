48e242cf9256b880221cc739168f3f6a1d56e937
sema karan <karan18sema@gmail.com>
2024-03-07 00:02:43 +0100

    add matches pages and match-cards functionality

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,13 @@
 class MatchesController < ApplicationController
+  before_action :authenticate_user!
+
+  # def index
+  #   @matches = Match.joins(:goal)
+  #                    .where('goals.user_id = ?', current_user.id)
+  # end
+
+  def index
+    @matches = Match.where('goal_id IN (?) OR matched_goal_id IN (?)', current_user.goals.pluck(:id), current_user.goals.pluck(:id))
+  end
+
 end
