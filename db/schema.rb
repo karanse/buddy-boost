eb762cfcf840e72174aa9341d83df4e620e63cf1
sema karan <karan18sema@gmail.com>
2024-03-06 12:13:00 +0100

    add matched column to goals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_100604) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_110222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_06_100604) do
     t.datetime "updated_at", null: false
     t.date "deadline"
     t.string "sub_category"
+    t.boolean "matched", default: false
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
