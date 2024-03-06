e41b3231132bf817d3c5c02b3c62764008aca960
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-03-06 16:07:59 +0100

    after call w/ta

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :photo
+  has_one_attached :photo, dependent: :destroy
+
   has_many :goals
   has_many :tasks, through: :goal
   has_many :matches
