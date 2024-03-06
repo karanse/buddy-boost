0b97258947f88fa0b4733b575dc3736a8e0b03c6
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-03-06 15:19:39 +0100

    update user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
   has_many :goals
   has_many :tasks, through: :goal
   has_many :matches
