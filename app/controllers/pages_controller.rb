f8348f2866e80133c45f9895577104c519423833
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-03-07 16:52:38 +0100

    avatars fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,7 @@ class PagesController < ApplicationController
     @goal = Goal.new
     # for buddy dashboards overview - index
     @matches = Match.where('goal_id IN (?) OR matched_goal_id IN (?)', current_user.goals.pluck(:id), current_user.goals.pluck(:id))
-    # @match = Match.find(params[:id]) >> did not work - show
+    # @match = Match.find(params[:id]) >> did not work
   end
 
   private
@@ -22,8 +22,8 @@ class PagesController < ApplicationController
     params.require(:goal).permit(:category, :description)
   end
 
-  def match_params
-    # params.require(:match).permit(:status, :matched_goal_id, :goal_id)
-  end
+  # def match_params
+  #   # params.require(:match).permit(:status, :matched_goal_id, :goal_id)
+  # end
 
 end
