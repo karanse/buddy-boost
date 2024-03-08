ce8801e4191f1b80ff20a194a04eef8b5011c57a
sema karan <karan18sema@gmail.com>
2024-03-08 14:05:22 +0100

    update JS setup

--- app/javascript/controllers/filter_subcategory_controller.js
+++ app/javascript/controllers/filter_subcategory_controller.js
@@ -1,4 +1,5 @@
-import { Controller } from 'stimulus'
+// import { Controller } from 'stimulus'
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["subcategory"]
