246e03f483854f9415d3e9c4b4eea48c9ed9f733
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-03-11 14:33:45 +0100

    edit button profile works

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -17,17 +17,24 @@ class GoalsController < ApplicationController
   end
 
   def edit
-    @goal = Goal.find(params[:id])
+    find_goal()
   end
 
   def update
+    find_goal()
+    @goal.update(goal_params)
+    redirect_to profile_path
     # code
   end
 
 private
 
+  def find_goal
+    @goal = Goal.find(params[:id])
+  end
+
   def goal_params
-    params.require(:goal).permit(:category, :description, :deadline)
+    params.require(:goal).permit(:category, :sub_category, :description, :deadline, :offline, :online)
   end
 
 end
