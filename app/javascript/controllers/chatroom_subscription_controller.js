027261ae0770466c8eb9aafea4b91e0f17b27a5e
allison.xuan.chen@gmail.com <allison.xuan.chen@gmail.com>
2024-03-12 11:28:25 +0100

    broadcasting

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+// Connects to data-controller="chatroom-subscription"
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
+  connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data) }
+    )
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+}
