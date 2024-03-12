d904dc34b309c67bb39630bfc2f8eb840f2e7df8
sema karan <karan18sema@gmail.com>
2024-03-12 16:29:46 +0100

    update task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,16 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+     @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to match_tasks_paths
+  end
+
   private
 
   def task_params
