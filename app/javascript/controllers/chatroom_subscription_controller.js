6f8c843ddb105871d13dfe81eac2a0d4ceda0d56
allison.xuan.chen@gmail.com <allison.xuan.chen@gmail.com>
2024-03-12 11:37:57 +0100

    scrolldown implemented

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -9,8 +9,13 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data) }
-    )
-    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
-  }
+      { received: data =>
+        this.#insertMessageAndScrollDown(data),
+        
+        #insertMessageAndScrollDown(data) {
+        this.messagesTarget.insertAdjacentHTML("beforeend", data)
+        this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+      },
+      console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`),
+  }),
 }
